6b8e4027b5221207ce6180cd7fdbbaf7887519ee
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-11-08 17:11:03 +0300

    Hot Fix 03

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -13,10 +13,8 @@ Usage:
     To run the application, execute this script directly.
 """
 
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
-
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -24,11 +22,29 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    """
+    Determine the best match with our supported languages.
+
+    This function uses request.accept_languages to determine the best match
+    with the supported languages defined in the app configuration.
+
+    Returns:
+        str: The best match for the supported languages.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
+    """
+    Render the index.html template with translated messages.
+
+    This function renders the 3-index.html template, passing translated strings
+    for home_title and home_header.
+
+    Returns:
+        str: The rendered HTML template.
+    """
     return render_template('3-index.html',
                            home_title=gettext('home_title'),
                            home_header=gettext('home_header'))
