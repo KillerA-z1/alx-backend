e13862f3f361c274cab62f691f9697585a2821ac
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-11-08 19:26:33 +0300

    Hot Fix 05

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -20,16 +20,14 @@ from flask_babel import Babel
 app = Flask(__name__)
 
 
-# Configure available languages and Babel
 class Config:
+    """ Configure available languages and Babel """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
