14cd47d03684fa6d4fb4050df8efe6fb17c114db
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-12-04 23:42:14 +0300

    Task 4.Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,28 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.message);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const hashKey = 'HolbertonSchools';
+
+client.hset(hashKey, 'Portland', 50, print);
+client.hset(hashKey, 'Seattle', 80, print);
+client.hset(hashKey, 'New York', 20, print);
+client.hset(hashKey, 'Bogota', 20, print);
+client.hset(hashKey, 'Cali', 40, print);
+client.hset(hashKey, 'Paris', 2, print);
+
+client.hgetall(hashKey, (err, result) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log(result);
+  }
+});
